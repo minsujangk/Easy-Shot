03855494fc9850bf270872a5c1aea2ef6d075218
nobleminsu <nobleminsu@gmail.com>
2017-01-12 15:56:01 +0900

    TODO 업데이트

--- app/src/main/java/doortodoor/easyshot/over_lollipop/mediaprojection/ScreenshotService.java
+++ app/src/main/java/doortodoor/easyshot/over_lollipop/mediaprojection/ScreenshotService.java
@@ -158,7 +158,6 @@ public class ScreenshotService extends Service {
 
         @Override
         public void onReceive(Context context, Intent intent) {
-            // TODO: This method is called when the BroadcastReceiver is receiving
             // an Intent broadcast.
             String action = intent.getAction();
             if (action.equals(INTENT_SCREENSHOT)) {
