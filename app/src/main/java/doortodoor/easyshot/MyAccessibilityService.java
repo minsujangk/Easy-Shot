e68f08a5aa593def3c09e10231e45127ece47397
nobleminsu <nobleminsu@gmail.com>
2017-01-14 11:31:10 +0900

    AccessibilityService 수정 및 테스트

--- app/src/main/java/doortodoor/easyshot/MyAccessibilityService.java
+++ app/src/main/java/doortodoor/easyshot/MyAccessibilityService.java
@@ -3,7 +3,11 @@ package doortodoor.easyshot;
 import android.accessibilityservice.AccessibilityService;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.Toast;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 import static android.content.ContentValues.TAG;
 
@@ -19,6 +23,20 @@ public class MyAccessibilityService extends AccessibilityService {
         Log.e(TAG, "Catch Event ContentDescription  : " + event.getContentDescription());
         Log.e(TAG, "Catch Event getSource : " + event.getSource());
         Log.e(TAG, "Catch Event getChildCount : " + event.getSource().getWindowId());
+        Queue<AccessibilityNodeInfo> queue = (Queue) new LinkedList();
+        for (int i = 0; i < event.getSource().getChildCount(); i++) {
+            queue.add(event.getSource().getChild(i));
+        }
+        while (!queue.isEmpty()) {
+            AccessibilityNodeInfo an = queue.poll();
+            if (an != null) {
+                if (an.getContentDescription() != null)
+                    Log.e(TAG, "Catch Event getChildCount : " + an.getContentDescription());
+                for (int i = 0; i < an.getChildCount(); i++) {
+                    queue.add(an.getChild(i));
+                }
+            }
+        }
         Log.e(TAG, "=========================================================================");
         final int eventType = event.getEventType();
         String eventText = null;
